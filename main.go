f28584ecdc1de775a00e8149254666a604eea79a
kido <kido@fenrir.co.jp>
2018-01-18 17:35:31 +0900

    home.html

--- main.go
+++ main.go
@@ -10,15 +10,31 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"os"
+	"text/template"
 )
 
 var addr = flag.String("addr", ":" + os.Getenv("PORT"), "http service address")
+var homeTempl = template.Must(template.ParseFiles("home.html"))
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	homeTempl.Execute(w, r.Host)
+}
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 	flag.Parse()
 	go h.run()
 	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome)
 	r.HandleFunc("/ws/{room}", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(w, r)
 	})
